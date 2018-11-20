a730274728e5f3ed6f7bedbfbb061f72704889d0
Zarconite <zarconite@gmail.com>
2018-11-20 10:39:23 +0000

    first commit

---
+++ src/returnHelloWorld/ReturnHelloWorld.java
@@ -0,0 +1,17 @@
+package returnHelloWorld;
+
+public class ReturnHelloWorld {
+	
+	public static void returnHelloWorld() {
+		
+		System.out.print("Hello World!");
+		
+	}
+	
+	public static void main(String[] args) {
+	
+		returnHelloWorld();
+		
+	}
+
+}
